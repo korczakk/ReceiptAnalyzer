ff3041a979fc4aadb75f3e656208c3a000d93d11
Kamil Korczak <kamil.korczak@edlund.dk>
2019-05-27 13:15:55 +0200

    Image preview completed

--- src/app/ReceiptImportModule/receipt-import/receipt-import.component.ts
+++ src/app/ReceiptImportModule/receipt-import/receipt-import.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-receipt-import',
   templateUrl: './receipt-import.component.html',
   styleUrls: ['./receipt-import.component.css']
 })
 export class ReceiptImportComponent implements OnInit {
+
+  public selectedImage;
+  public fileName: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public createPreview(file): void {
+    if (!file)
+      return;
+
+    if (file.type.indexOf('image') == -1)
+      alert('Pliku musi być grafiką.');
+
+    this.fileName = file.name;
+
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = (_event) => {
+      this.selectedImage = reader.result;
+    };
+  }
 }
