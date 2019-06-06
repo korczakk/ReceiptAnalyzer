4a12cb1de3181694d55113d780c9246bcdc5060d
Kamil Kkorczak <kamil.korczak@gmail.com>
2019-06-06 21:41:00 +0200

    Azure OCR services in progress

--- ReceiptAnalyzer.Functions/ImageUploadFunction/Function1.cs
+++ ReceiptAnalyzer.Functions/ImageUploadFunction/Function1.cs
@@ -10,24 +10,21 @@ using Newtonsoft.Json;
 
 namespace ImageUploadFunction
 {
-    public static class Function1
+  public static class ImageUploader
+  {
+    [FunctionName("UploadImage")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+      log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+      string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+      dynamic data = JsonConvert.DeserializeObject(requestBody);
+   
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-        }
+      return new OkResult();
     }
+  }
 }
