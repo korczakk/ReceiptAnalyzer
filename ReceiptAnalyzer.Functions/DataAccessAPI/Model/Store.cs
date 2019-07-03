2b3d20660f99ab0ee134816c82e9a883e0385619
Kamil Kkorczak <kamil.korczak@gmail.com>
2019-07-03 23:29:51 +0200

    GetStores function ready

--- ReceiptAnalyzer.Functions/DataAccessAPI/Model/Store.cs
+++ ReceiptAnalyzer.Functions/DataAccessAPI/Model/Store.cs
@@ -2,13 +2,14 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccessAPI.Model
 {
   public class Store : ITableEntity
   {
-    public string storeName { get; set; }
+    public string StoreName { get; set; }
     public string PartitionKey { get ; set ; }
     public string RowKey { get; set ; }
     public DateTimeOffset Timestamp { get ; set ; }
@@ -16,7 +17,7 @@ namespace DataAccessAPI.Model
 
     public void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
     {
-      
+      StoreName = properties.Values.SingleOrDefault().StringValue;
     }
 
     public IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
