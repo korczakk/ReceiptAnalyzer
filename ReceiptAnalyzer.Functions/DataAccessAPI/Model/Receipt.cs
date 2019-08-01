4023d7dcec3ce50a571745b6d35a0d1830b8011a
Kamil Korczak <kamil.korczak@edlund.dk>
2019-08-01 14:10:51 +0200

    Function to add receipt

---
+++ ReceiptAnalyzer.Functions/DataAccessAPI/Model/Receipt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+using Newtonsoft.Json;
+
+namespace DataAccessAPI.Model
+{
+  public class Receipt : TableEntity
+  {
+    public Store store { get; set; }
+    public DateTime shoppingDate { get; set; }
+    public double TotalAmount { get; set; }
+    public List<ReceiptItem> Items { get; set; }
+
+    public new void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+    {
+      if (properties.Values.SingleOrDefault().StringValue is string store)
+      {
+        this.store = JsonConvert.DeserializeObject<Store>(store);
+      }
+
+      this.shoppingDate = (DateTime) properties.Values.SingleOrDefault().DateTime;
+      this.TotalAmount = (double) properties.Values.SingleOrDefault().DoubleValue;
+
+      if (properties.Values.SingleOrDefault().StringValue is string items)
+      {
+        this.Items = (List<ReceiptItem>)JsonConvert.DeserializeObject<List<ReceiptItem>>(items);
+      }
+    }
+
+    public new IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
+    {
+      EntityProperty store = EntityProperty.GeneratePropertyForString(JsonConvert.SerializeObject(this.store));
+
+      var propertiesDictionary = new Dictionary<string, EntityProperty>();
+      propertiesDictionary.Add("store", store);
+
+      return propertiesDictionary;
+    }
+  }
+}
